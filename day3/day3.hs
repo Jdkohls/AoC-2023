f8053852ab41e3195422871f9f1e201235981787
Jackson Kohls <jdkohls@g.ucla.edu>
2023-12-04 00:52:09 +0000

    day 3 done.

--- day3/day3.hs
+++ day3/day3.hs
@@ -9,10 +9,14 @@ import Debug.Trace
 main :: IO ()
 main = do
     contents <- readFile "input.txt"
-    print . sum . preprocessParse $ contents
+    print . sum . parse . cleanup $ contents
+    print . sum . parse' . cleanup $ contents
 
 debug :: c -> String -> c
 debug = flip trace
+
+cleanup :: [Char] -> [Char]
+cleanup = join . splitOn "\n"
 
 getSymbols :: [Char] -> [Char] --   +%=#/@$&*-
 getSymbols xs = getSymbols' [] xs
@@ -61,7 +65,60 @@ parse xs = fn (getNumIndexRange xs) (getSymbolNums xs $ getSymbols xs) --`debug`
             | h < y            = fn xs (y:ys)  --`debug` (" " ++ show y  ++ " " ++ show h ++ "\n") -- goto next num 
             | otherwise        = val : fn xs ys --`debug` "next"
 
-preprocessParse :: [Char] -> [Int]
-preprocessParse = parse . join . splitOn "\n"
 
-getStars = getSymbolIndex "*" 
+
+
+getStars :: [Char] -> [[Int]]
+getStars xs = map (sort . pm getLineLength) (pm' 1 (getSymbolIndex "*" xs))
+    where
+        pm' :: Int -> [Int]  -> [[Int]]
+        pm' _ []  = []
+        pm' i (x:xs) = [x - i, x, x + i]:pm' i xs
+
+        pm :: Int -> [Int]  -> [Int]
+        pm _ []  = []
+        pm i (x:xs) = (x - i):x:(x+i):pm i xs
+
+starmap :: [Char] -> [Int]
+starmap = flip getSymbolNums "*"
+
+findNearbyStars :: [Char] -> [[Int]]
+findNearbyStars xs = fn (getNumIndexRange xs) (starmap xs)
+    where
+        fn :: [[Int]] -> [Int] -> [[Int]]
+        fn [] _ = []
+        fn _ [] = []
+        fn xp@([val, l, h]:xs) yp@(y:ys)
+            | l > y            = fn xp ys -- goto next valid symbol space
+            | h < y            = fn xs yp  --`debug` (" " ++ show y  ++ " " ++ show h ++ "\n") -- goto next num 
+            | otherwise        = [y,val] : fn xs ys
+
+
+fltr :: [[Int]] -> [Int]
+fltr [] = []
+fltr xs = fltr' False 0 (-1) [] xs
+    where
+        fltr' _ _ _ acc [] = acc
+        fltr' polarity num seen acc xp@([star,val]:xs)   
+            | star == seen && polarity = (num*val):(fltr xs)
+            | otherwise                = fltr' True val star acc xs
+
+
+findTrue :: (a -> [a] -> Bool) -> a -> [[a]] -> Int
+findTrue f cond li = findTrue' f cond li 0
+    where
+        findTrue' f cond (x:xs) i = if f cond x then i else findTrue' f cond xs (i+1)
+        findTrue' f cond [] _ = (-1)
+
+
+
+--parse' :: [Char] -> [Int]
+parse' [] = []
+parse' xs =  fltr (sortBy (\[a,_] [b,_] -> compare a b) (findStar (findNearbyStars xs) (getStars xs)))  --`debug` (show (findStar (findNearbyStars xs) (getStars xs)) ++ "\n" ++ show (findNearbyStars xs)  ++ "\n") -- goto next num
+    where
+        findStar :: [[Int]] -> [[Int]] -> [[Int]]
+        findStar _ [] = []
+        findStar [] _ = []
+        findStar ([starIndex, val]:xs) ys = [findTrue elem starIndex ys, val]:findStar xs ys
+
+
